2c61cf7d78168d771a889f4a619e1521873cb110
kurohikari <arthur.geneau@gmail.com>
2017-03-25 02:07:14 +0800

    Added Show Controller

---
+++ P3/src/controller/ShowController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import model.Show;
+
+import java.util.List;
+
+/**
+ * Created by arthur on 3/25/2017.
+ */
+public class ShowController {
+
+    public ShowController() {
+
+    }
+
+    public void createShow(String name, String city, String date, String time, String weblink) {
+        //TODO create a show and add it to the database
+    }
+
+    public void deleteShow() {
+        //TODO remove a show from database
+    }
+
+    public void modifyShow(int id) {
+        //TODO modify the show's information in the database
+    }
+
+    public List<Show> searchShowsByName(String query) {
+        //TODO return a list of shows that correspond to the query based on their names
+        return null;
+    }
+
+    public List<Show> searchShowsByDate(String query) {
+        //TODO return a list of shows that correspond to the query based on their dates
+        return null;
+    }
+
+    public List<Show> searchShowsByCity(String query) {
+        //TODO return a list of shows that correspond to the query based on their cities
+        return null;
+    }
+
+}
